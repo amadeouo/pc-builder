fb6e7b068c10adddddcfbc65bf7e9cb8ac812b74
Mikhail Volodko <clavor.32@gmail.com>
2026-05-07 19:45:55 +0300

    init: init project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/shadcn-ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Button>Привет</Button>
+      </main>
+    </div>
+  );
+}
