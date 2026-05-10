712a47f9d0b80d45ca5bb63e877337646f147883
Mikhail Volodko <clavor.32@gmail.com>
2026-05-10 07:06:53 +0300

    feat: secure the private route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,14 @@
 import { LoginForm } from "@/components/login-form"
+import { auth } from "@/model/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth()
+
+  if (session) {
+    redirect("/dashboard")
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
