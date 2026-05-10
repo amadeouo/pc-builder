9a1880970435cdcbe22d518548a3ab910d62c2cf
Mikhail Volodko <clavor.32@gmail.com>
2026-05-10 10:57:29 +0300

    fix: fix memory leak from matcher in proxy with uncorrect regex

--- src/proxy.ts
+++ src/proxy.ts
@@ -28,6 +28,6 @@ export default function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|.*\\.(?:png|jpg|jpeg|gif|svg)$).*)',
+    '/((?!api|_next/static|_next/image|.*\\.png$).*)',
   ],
 }
