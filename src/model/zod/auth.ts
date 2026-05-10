f4290c5768833494495c34732322acf7cc3d74ff
Mikhail Volodko <clavor.32@gmail.com>
2026-05-10 06:46:17 +0300

    fix: update error message

--- src/model/zod/auth.ts
+++ src/model/zod/auth.ts
@@ -1,15 +1,20 @@
 import { z } from 'zod';
 
 export const LoginSchema = z.object({
-  email: z.email({ error: "Неправильный формат почты" }),
+  email: z.string({ error: "Поле не может быть пустым" })
+    .email({ error: "Неправильный формат почты" }),
   password: z.string({ error: "Поле не может быть пустым"}),
 })
 
 export const SignupSchema = z.object({
-  name: z.string({ error: "Поле не может быть пустым"}).min(2, { error: "Имя должно содержать 2 или больше символов" }),
-  email: z.string({ error: "Поле не может быть пустым"}).email({ error: "Неправильный формат почты" }),
-  password: z.string({ error: "Поле не может быть пустым"}).min(8),
-  repeatPassword: z.string({ error: "Поле не может быть пустым"}).min(8)
+  name: z.string({ error: "Поле не может быть пустым"})
+    .min(2, { error: "Имя должно содержать 2 или больше символов" }),
+  email: z.string({ error: "Поле не может быть пустым"})
+    .email({ error: "Неправильный формат почты" }),
+  password: z.string({ error: "Поле не может быть пустым"})
+    .min(8, { error: "Пароль должен содержать больше 8 символов" }),
+  repeatPassword: z.string({ error: "Поле не может быть пустым"})
+    .min(8, { error: "Пароль должен содержать больше 8 символов" })
 }).refine(values => values.password === values.repeatPassword, {
   error: "Пароли должны совпадать",
   path: ["repeatPassword"]
